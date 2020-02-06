342d9056608e8642ccf72f9020bcc3f2301c9fbd
Leetaeheon <pengdoong95@gmail.com>
2020-02-06 12:48:37 +0900

    ch03 추가

---
+++ 01.Basic/ch03/src/C_EscapeSequences.java
@@ -0,0 +1,11 @@
+
+public class C_EscapeSequences {
+
+	public static void main(String[] args) {
+		System.out.println("AB" + '\b' + 'C');
+        System.out.println("AB" + '\t' + 'C');
+        System.out.println("AB" + '\n' + 'C');
+        System.out.println("AB" + '\r' + 'C');
+	}
+
+}
---
+++ 01.Basic/ch03/src/D_Conversion.java
@@ -0,0 +1,23 @@
+
+public class D_Conversion {
+
+	public static void main(String[] args) {
+		double pi = 3.1415;
+		int wholeNumber = (int)pi;
+		
+		System.out.println(wholeNumber);
+		
+		long num1 = 300000007L;
+		int num2 = (int)num1;
+		
+	    System.out.println(num2);
+	    
+	    short num3 = 1;
+	    short num4 = 2;
+	    short num5 = (short)(num3 + num4);
+	    
+	    System.out.println(num5);
+
+	}
+
+}
