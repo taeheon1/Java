eb5eda01d1d0fb5957f92953f599e993bcd97092
Leetaeheon <pengdoong95@gmail.com>
2020-02-07 19:59:53 +0900

    ch06 추가

---
+++ 01.Basic/ch06/src/B2_SwitchBreak.java
@@ -0,0 +1,24 @@
+
+public class B2_SwitchBreak {
+
+	public static void main(String[] args) {
+		int n = 2;
+		
+		switch(n) {
+		case 1:
+			System.out.println("Simple Java");
+			break;
+		case 2:
+			System.out.println("Funny Java");
+			break;
+		case 3:
+			System.out.println("Fantastic Java");
+			break;
+		default :
+			System.out.println("The best programing language");
+			
+		}
+		System.out.println("Do you like Java?");
+	}
+
+}
---
+++ 01.Basic/ch06/src/C1_WhileBasic.java
@@ -0,0 +1,14 @@
+
+public class C1_WhileBasic {
+
+	public static void main(String[] args) {
+		int num = 0;
+		
+		while(num < 5) {
+			System.out.println("I like Java" + num);
+			num++;
+		}
+
+	}
+
+}
